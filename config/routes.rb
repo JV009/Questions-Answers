098f08854792ad983d0c344f07f5cac39055943b
JV009 <krysov_aleksandr@mail.ru>
2025-03-01 15:43:43 +0300

    Added answers controller with tests

--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,7 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "posts#index"
 
-  resources :questions
+  resources :questions do
+    resources :answers
+  end
 end
